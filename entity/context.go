ffaa7a892f17fb6e0eaa7099d02baff7ed0b164d
Paulo Borges <pborges475@gmail.com>
2022-07-31 12:24:22 -0700

    delete unused context

--- entity/context.go
+++ entity/context.go
@@ -4,11 +4,6 @@ type Closer interface {
 	Close()
 }
 
-type Context interface {
-	Notify(method string, param any)
-	OnDisconnect(fn func())
-}
-
 type CanRegisterSubscription interface {
 	RegisterSubscription(id string, sub Closer)
 }
